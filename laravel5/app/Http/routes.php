064e220f63018a0090cb07bc659709c01308fa03
Hanif Aulia <cinema.hanifaulia@gmail.com>
2017-03-11 21:18:23 +0800

    posttest 2

--- laravel5/app/Http/routes.php
+++ laravel5/app/Http/routes.php
@@ -16,6 +16,17 @@ Route::get('/', function () {
     $nim = '1515015012';
     return view('welcome', compact('nama', 'nim'));
 });
+Route::get('/postest1',function(){
+    $nama = 'Hanif Aulia';
+    $nim = '1515015012';
+    return view('welcome', compact('nama', 'nim'));
+});
+Route::get('pengguna/{pengguna}',function($pengguna){
+    return "hallo world dari pengguna $pengguna";
+});
+Route::get('berita/{berita?}',function($berita="laravel 5"){
+    return "berita $berita belum dibaca";
+});
 //Route('group', ['prefix' => 'Posttest', '']);
 Route::get('mahasiswa',['namespace' => 'Posttest', 'uses' => 'Posttest\Tugas1Controller@namanim']);
 Route::get('/mahasiswa1', ['uses'=>'mahasiswa1Controller@data_mahasiswa']);
